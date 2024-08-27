66dd21d4ec4fbdecafe5885532e6f35249f2b159
Divya Karade <72258493+DivyaKarade@users.noreply.github.com>
2024-08-27 17:09:22 +0530

    Update AutoDL.py

--- AutoDL.py
+++ AutoDL.py
@@ -1,209 +1,143 @@
+import streamlit as st
 import pandas as pd
-import autokeras as ak
-from autokeras import ImageClassifier, ImageRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_score, recall_score, roc_auc_score, f1_score, confusion_matrix
 import numpy as np
 import tensorflow as tf
-import streamlit as st
-import base64
-import io
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    confusion_matrix, precision_score, recall_score, f1_score, roc_auc_score
+)
+from autokeras import StructuredDataClassifier, StructuredDataRegressor
 import matplotlib.pyplot as plt
-import math
 
-# Page expands to full width
-st.set_page_config(page_title='AIDrugApp', page_icon='🌐', layout="wide")
+st.title("AutoKeras Tabular Data Modeling")
 
-# For hiding streamlit messages
-#st.set_option('deprecation.showPyplotGlobalUse', False)
-#st.set_option('deprecation.showfileUploaderEncoding', False)
+# Sidebar options
+st.sidebar.title("Options")
+add_selectbox = st.sidebar.selectbox("Choose a task", ("Classification", "Regression"))
 
-# Create title and subtitle
-html_temp = """
-        <div style="background-color:teal">
-        <h1 style="font-family:arial;color:white;text-align:center;">AIDrugApp</h1>
-        <h4 style="font-family:arial;color:white;text-align:center;">Artificial Intelligence Based Virtual Screening Web-App for Drug Discovery</h4>
-        </div>
-        <br>
-        """
-st.markdown(html_temp, unsafe_allow_html=True)
+# Upload dataset
+uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head())
 
-st.sidebar.title("AIDrugApp v1.2.6")
-st.sidebar.header("Menu")
-CB = st.sidebar.checkbox("Auto-DL")
+    # Data preprocessing
+    target = st.sidebar.selectbox("Select the target column", data.columns)
+    X = data.drop(columns=[target])
+    y = data[target]
 
-if CB:
-    st.title('Auto-DL')
-    st.success(
-        "This module of [**AIDrugApp v1.2.6**](https://aidrugapp.streamlit.app/) helps to build the best Deep Learning model on user data."
-        " It also helps to predict target data using the same deep learning algorithm.")
+    test_size = st.sidebar.slider("Test Size (fraction)", 0.1, 0.5, 0.2)
+    random_state = st.sidebar.number_input("Random State (for reproducibility)", value=42)
 
-    expander_bar = st.expander("👉 More information")
-    expander_bar.markdown("""
-            * **Python libraries:** Tensorflow, AutoKeras, scikit-learn, streamlit, pandas, numpy, matplotlib
-            * **Publication:** 1. Divya Karade. (2021). Custom ML Module of AIDrugApp for Molecular Identification, Descriptor Calculation, and Building ML/DL QSAR Models. [ChemRxiv Preprint](https://doi.org/10.33774/chemrxiv-2021-3f1f9).
-            2. Divya Karade. (2021, March 23). AutoDL: Automated Deep Learning (Machine learning module of AIDrugApp - Artificial Intelligence Based Virtual Screening Web-App for Drug Discovery) (Version 1.0.0). [Zenodo](http://doi.org/10.5281/zenodo.4630119)
-            """)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-    expander_bar = st.expander("👉 How to use Auto-DL?")
-    expander_bar.markdown("""**Step 1:** In the User input-side panel, select the type of algorithm ('Classification' or 'Regression') for building the DL model.""")
-    expander_bar.markdown("""**Step 2:** Upload descriptor data (included with target data) for building the DL model. (*Example input file provided*)""")
-    expander_bar.markdown("""**Step 3:** For developing the model, specify parameters such as 'Train-Test split percent', 'random seed number', 'maximum trial number', and 'epochs number'.""")
-    expander_bar.markdown("""**Step 4:** Upload descriptor data (excluded with target data) for making target predictions. (*Example input file provided*)""")
-    expander_bar.markdown("""**Step 5:** Click the "✨ PREDICT" button and the results will be displayed below to view and download.""")
+    st.write("Training set size:", X_train.shape)
+    st.write("Test set size:", X_test.shape)
 
-    """---"""
+    # Model training
+    if add_selectbox == 'Classification':
+        st.subheader("AutoKeras Structured Data Classification")
 
-    st.sidebar.header('⚙️ USER INPUT PANEL')
-    st.sidebar.write('**1. Which Auto-DL algorithm would you like to select for building DL models?**')
-    add_selectbox = st.sidebar.radio(
-        "Select your algorithm",
-        ("Regression", "Classification"))
+        max_trials = st.sidebar.slider("Max Trials", 1, 100, 10)
+        epochs = st.sidebar.slider("Epochs", 1, 100, 10)
+        seed_number = st.sidebar.number_input("Seed number", value=42)
 
-    st.sidebar.write('**2. Upload data file for building deep learning models**')
-    uploaded_file = st.sidebar.file_uploader("Upload input .csv file", type=["csv"])
-    st.sidebar.markdown("""[Example .csv input file](https://github.com/DivyaKarade/Example-.csv-input-files--AIDrugApp-v1.2/tree/main/Example-.csv-input-files_AutoDL)""")
+        tf.random.set_seed(seed_number)
+        np.random.seed(seed_number)
 
-    # Sidebar - Specify parameter settings
-    st.sidebar.write('**3. Set Parameters**')
-    split_size = st.sidebar.number_input('Train-Test split %', 0, 100, 70, 5)
-    seed_number = st.sidebar.number_input('Set the random seed number', 1, 100, 42, 1)
-    max_trials = st.sidebar.number_input('Set the maximum trial number', 1, 100, 15, 1)
-    epochs = st.sidebar.number_input('Set the epochs number', 1, 1000, 50, 5)
+        search = StructuredDataClassifier(max_trials=max_trials)
+        search.fit(x=X_train, y=y_train, verbose=0, epochs=epochs)
 
-    st.sidebar.write("**4. Upload data file for predictions: **")
-    file_upload = st.sidebar.file_uploader("Upload .csv file", type=["csv"])
-    st.sidebar.markdown("""[Example .csv input file](https://github.com/DivyaKarade/Example-.csv-input-files--AIDrugApp-v1.2/tree/main/Example-.csv-input-files_AutoDL)""")
+        y_pred_train = search.predict(X_train)
+        y_pred_test = search.predict(X_test)
 
-    if file_upload is not None:
-        data = pd.read_csv(file_upload)
-        st.info("**Uploaded data for making predictions **")
-        st.write('Data Dimension: ' + str(data.shape[0]) + ' rows and ' + str(data.shape[1]) + ' columns.')
-        st.write(data.style.highlight_max(axis=0))
-    else:
-        st.info('Awaiting .csv file to be uploaded for making predictions')
+        model = search.export_model()
 
-    DA = st.sidebar.button("✨ PREDICT")
+        st.info('**Model summary**')
+        st.write(model.summary())
 
-    # Load dataset
-    if uploaded_file is not None:
-        data_1 = pd.read_csv(uploaded_file)
-        X = data_1.iloc[:, :-1]  # Using all columns except for the last column as X
-        Y = data_1.iloc[:, -1]  # Selecting the last column as Y
+        st.info('**Model evaluation - Training Set**')
+        loss, acc = search.evaluate(X_train, y_train, verbose=0)
+        st.write('Accuracy: %.3f' % acc)
+        precision = precision_score(y_train, y_pred_train, average="weighted")
+        st.write('Precision: %f' % precision)
+        recall = recall_score(y_train, y_pred_train, average="weighted")
+        st.write('Sensitivity/Recall: %f' % recall)
+        f1 = f1_score(y_train, y_pred_train, average="weighted")
+        st.write('F1 score: %f' % f1)
+        auc = roc_auc_score(y_train, search.predict_proba(X_train), multi_class='ovo')
+        st.write('ROC AUC: %f' % auc)
+        st.write("Confusion matrix")
+        matrix = confusion_matrix(y_train, y_pred_train)
+        st.write(matrix)
 
-        st.info("**Uploaded data for building DL models: **")
-        st.write('Data Dimension: ' + str(data_1.shape[0]) + ' rows and ' + str(data_1.shape[1]) + ' columns.')
-        st.write(data_1.style.highlight_max(axis=0))
+        st.info('**Model evaluation - Test Set**')
+        loss, acc = search.evaluate(X_test, y_test, verbose=0)
+        st.write('Accuracy: %.3f' % acc)
+        precision = precision_score(y_test, y_pred_test, average="weighted")
+        st.write('Precision: %f' % precision)
+        recall1 = recall_score(y_test, y_pred_test, average="weighted")
+        st.write('Sensitivity/Recall: %f' % recall1)
+        f1 = f1_score(y_test, y_pred_test, average="weighted")
+        st.write('F1 score: %f' % f1)
+        auc = roc_auc_score(y_test, search.predict_proba(X_test), multi_class='ovo')
+        st.write('ROC AUC: %f' % auc)
+        st.write("Confusion matrix")
+        matrix = confusion_matrix(y_test, y_pred_test)
+        st.write(matrix)
 
-        # Data split
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=(1 - split_size / 100),
-                                                            random_state=seed_number)
+    elif add_selectbox == 'Regression':
+        st.subheader("AutoKeras Structured Data Regression")
 
-        st.write('**Training set**')
-        Train = pd.concat([X_train, y_train], axis=1)
-        st.write('Data Dimension: ' + str(Train.shape[0]) + ' rows and ' + str(Train.shape[1]) + ' columns.')
-        st.write(Train)
-        st.download_button('Download CSV', Train.to_csv(index=False), 'Train.csv', 'text/csv')
+        max_trials = st.sidebar.slider("Max Trials", 1, 100, 10)
+        epochs = st.sidebar.slider("Epochs", 1, 100, 10)
+        seed_number = st.sidebar.number_input("Seed number", value=42)
 
-        st.write('**Test set**')
-        Test = pd.concat([X_test, y_test], axis=1)
-        st.write('Data Dimension: ' + str(Test.shape[0]) + ' rows and ' + str(Test.shape[1]) + ' columns.')
-        st.write(Test)
-        st.download_button('Download CSV', Test.to_csv(index=False), 'Test.csv', 'text/csv')
+        tf.random.set_seed(seed_number)
+        np.random.seed(seed_number)
 
-        if add_selectbox == 'Classification':
-            if DA:
-                tf.random.set_seed(seed_number)
-                np.random.seed(seed_number)
+        search = StructuredDataRegressor(max_trials=max_trials)
+        search.fit(x=X_train, y=y_train, verbose=0, epochs=epochs)
 
-                search = ImageClassifier(max_trials=max_trials)
-                search.fit(x=X_train, y=y_train, verbose=0, epochs=epochs)
+        y_pred_train = search.predict(X_train)
+        y_pred_test = search.predict(X_test)
 
-                y_pred_train = search.predict(X_train)
-                y_pred_test = search.predict(X_test)
+        model = search.export_model()
 
-                model = search.export_model()
+        st.info('**Model summary**')
+        st.write(model.summary())
 
-                st.info('**Model summary**')
-                st.write(model.summary())
+        st.info('**Model evaluation - Training Set**')
+        loss = search.evaluate(X_train, y_train, verbose=0)
+        st.write('Mean Squared Error: %.3f' % loss)
 
-                st.info('**Model evaluation - Training Set**')
-                loss, acc = search.evaluate(X_train, y_train, verbose=0)
-                st.write('Accuracy: %.3f' % acc)
-                precision = precision_score(y_train, y_pred_train)
-                st.write('Precision: %f' % precision)
-                recall = recall_score(y_train, y_pred_train)
-                st.write('Sensitivity/Recall: %f' % recall)
-                f1 = f1_score(y_train, y_pred_train)
-                st.write('F1 score: %f' % f1)
-                auc = roc_auc_score(y_train, y_pred_train)
-                st.write('ROC AUC: %f' % auc)
-                st.write("Confusion matrix")
-                matrix = confusion_matrix(y_train, y_pred_train)
-                st.write(matrix)
+        st.info('**Model evaluation - Test Set**')
+        loss = search.evaluate(X_test, y_test, verbose=0)
+        st.write('Mean Squared Error: %.3f' % loss)
 
-                st.info('**Model evaluation - Test Set**')
-                loss, acc = search.evaluate(X_test, y_test, verbose=0)
-                st.write('Accuracy: %.3f' % acc)
-                precision = precision_score(y_test, y_pred_test)
-                st.write('Precision: %f' % precision)
-                recall1 = recall_score(y_test, y_pred_test)
-                st.write('Sensitivity/Recall: %f' % recall1)
-                f1 = f1_score(y_test, y_pred_test)
-                st.write('F1 score: %f' % f1)
-                auc = roc_auc_score(y_test, y_pred_test)
-                st.write('ROC AUC: %f' % auc)
-                st.write("Confusion matrix")
-                matrix = confusion_matrix(y_test, y_pred_test)
-                st.write(matrix)
+        # Error Distribution
+        st.info('**Error Distribution - Test Set**')
+        Error = pd.DataFrame(y_test)
+        Error['Prediction'] = y_pred_test
+        Error['Error'] = Error['Prediction'] - Error.iloc[:, 0]
 
-        elif add_selectbox == 'Regression':
-            if DA:
-                tf.random.set_seed(seed_number)
-                np.random.seed(seed_number)
+        st.write(Error)
 
-                search = ImageRegressor(max_trials=max_trials)
-                search.fit(x=X_train, y=y_train, verbose=0, epochs=epochs)
+        st.info('**Plot of Prediction vs Real Value**')
+        plt.figure(figsize=(10, 6))
+        plt.scatter(y_test, y_pred_test)
+        plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red')
+        plt.xlabel('Real Value')
+        plt.ylabel('Prediction')
+        plt.title('Prediction vs Real Value')
+        st.pyplot(plt)
 
-                y_pred_train = search.predict(X_train)
-                y_pred_test = search.predict(X_test)
-
-                model = search.export_model()
-
-                st.info('**Model summary**')
-                st.write(model.summary())
-
-                st.info('**Model evaluation - Training Set**')
-                loss = search.evaluate(X_train, y_train, verbose=0)
-                st.write('Mean Squared Error: %.3f' % loss)
-
-                st.info('**Model evaluation - Test Set**')
-                loss = search.evaluate(X_test, y_test, verbose=0)
-                st.write('Mean Squared Error: %.3f' % loss)
-
-                # Error Distribution
-                st.info('**Error Distribution - Test Set**')
-                Error = pd.DataFrame(y_test)
-                Error['Prediction'] = y_pred_test
-                Error['Error'] = Error['Prediction'] - Error.iloc[:, 0]
-
-                st.write(Error)
-
-                st.info('**Plot of Prediction vs Real Value**')
-                plt.figure(figsize=(10, 6))
-                plt.scatter(y_test, y_pred_test)
-                plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red')
-                plt.xlabel('Real Value')
-                plt.ylabel('Prediction')
-                plt.title('Prediction vs Real Value')
-                st.pyplot(plt)
-
-                st.info('**Plot of Error Distribution**')
-                plt.figure(figsize=(10, 6))
-                plt.hist(Error['Error'], bins=25)
-                plt.xlabel('Prediction Error')
-                plt.ylabel('Count')
-                st.pyplot(plt)
+        st.info('**Plot of Error Distribution**')
+        plt.figure(figsize=(10, 6))
+        plt.hist(Error['Error'], bins=25)
+        plt.xlabel('Prediction Error')
+        plt.ylabel('Count')
+        st.pyplot(plt)
 
 else:
     st.info('Awaiting .csv file to be uploaded. A sample dataset is available in the sidebar.')
